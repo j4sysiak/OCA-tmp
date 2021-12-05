c4aec89a0c4ee62e8bf7b46658878550541b7519
Jacek <jaceksysiak@wp.pl>
2021-12-05 07:35:33 +0100

    232112

--- src/com/udayan/oca/Dziedziczenie_info_Employee_Manager/Test.java
+++ src/com/udayan/oca/Dziedziczenie_info_Employee_Manager/Test.java
@@ -57,9 +57,9 @@ public class Test {
 //        AbstractList<String> aList = (AbstractList)list2;
 
 
-        C2_Employee obj1 = new C1_Manager();  //niepełny obiekt Manager  (pokaże tylko metody @Override)
+        C2_Employee obj1 = new C1_Manager();  //niepełny obiekt Manager (zbudowany na fundamentach Employee)  (pokaże tylko w Manager metody @Override, a w Employee wszystkie metody)
      //   I obj2 = new C2_Employee();  //'C2_Employee' is abstract; cannot be instantiated
-        I obj2 = new C1_Manager();  //niepełny obiekt Managera (pokaże tylko metody zaimplementowane w Manager lub Employee z interfejsu I)
+        I obj2 = new C1_Manager();  //niepełny obiekt Managera (zbudowany na fundamentach I) (pokaże tylko metody (z interfejsu I) zaimplementowane w Manager lub Employee )
         //int x=obj2.salary;   // XXXXXXX niewidoczny XXXXXX
         //obj2.projektyManageraTopSecret();  // XXXXXXX niewidoczny XXXXXX
         //obj2.displayC2(); // XXXXXXX niewidoczny XXXXXX
@@ -67,7 +67,7 @@ public class Test {
         //obj2.bhp();       // XXXXXXX niewidoczny XXXXXX
 
         // C2_Employee s = obj2;  // Tylko w przypadku interfejsu: XXXXXXXXX Required type: C2_Employee  Provided: I  XXXXXXXXXXXXXX
-        C2_Employee s = (C2_Employee)obj2;   //  s to będzie niepełny obiekt Managera
+        C2_Employee s = (C2_Employee)obj2;   //  s to będzie typowy niepełny obiekt Managera  (zbudowany z poziomu interface na fundamentach Employee)
         int x = s.salary;  // odpalany zawsze w Employee
         s.displayC2();     // odpalany w Employee, bo nie ma w @Override Manager
         s.displayI();      // odpalany w Employee, bo nie ma w @Override Manager
@@ -75,11 +75,11 @@ public class Test {
         // s.projektyManageraTopSecret()  XXXXXXX niewidoczny - dostęp tylko dla pełnego objektu Manager XXXXXX
 
 
-        I t = obj1;  // t -obiekt interfejsu I (widoczne tylko metody zaimplentowane z interfejsu) - obj1 - niepełny Manager
+        I t = obj1;  // t -obiekt interfejsu I (widoczne tylko metody zaimplentowane z interfejsu) - obj1 - niepełny Manager (zbudowany na fundamentach Employee)
         t.displayI();
 
 
-        C1_Manager manager = new C1_Manager();  //pełny Manager: odpala wszystko z Employee jeżeli nie ma Override u siebie, a jeżeli jest Overriden to je odpala
+        C1_Manager manager = new C1_Manager();  //pełny Manager: odpala wszystko z Employee, jeżeli nie ma @Override u siebie, a jeżeli ma @Overriden to je odpala u siebie
         x=manager.salary;    // odpalany zawsze w Manager
         manager.projektyManageraTopSecret();  // odpalany w Manager bo nie ma w Employee
         manager.displayC2(); // odpalany z Employee bo nie ma w Manager
@@ -87,7 +87,7 @@ public class Test {
         manager.bhp();       // odpalany z Manager bo jest @Override
 
 
-        C2_Employee emplManag = new C1_Manager(); // niepełny Manager, nie odpali metody w klasie Manager jeżeli metoda nie jest @override
+        C2_Employee emplManag = new C1_Manager(); // niepełny Manager (na fundamentach Employee), nie odpali metody w klasie Manager jeżeli metoda nie jest @override
         x=emplManag.salary;    // odpalany zawsze w Employee
         emplManag.displayI();  // odpalany w Employee bo nie jest @Override w Manager
         emplManag.displayC2(); // odpalany w Employee bo nie jest @Override w Manager
@@ -97,7 +97,7 @@ public class Test {
 
         // byte <- (byte)short <- (short)int <- (int)long <- (long)float <- (float)double
         // Manager <- (Manager)Employee <- (Employee)Object
-        manager = (C1_Manager) emplManag;   //obiekt manager jest nadal pełnym obiektem Manager
+        manager = (C1_Manager) emplManag;   //obiekt Manager: jest nadal pełnym obiektem Manager
         x=manager.salary;     // tak jak zwykły manager - nic się nie zmienia
         manager.projektyManageraTopSecret();  // tak jak zwykły manager - nic się nie zmienia
         manager.displayC2();  // tak jak zwykły manager - nic się nie zmienia
