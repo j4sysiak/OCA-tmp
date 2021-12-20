7c8fcd84a6d1f7986adec287c06c0ff3b270fa0d
Jacek <jaceksysiak@wp.pl>
2021-12-20 07:13:17 +0100

    20-12-2021 09:00

--- src/com/udayan/oca/Test.java
+++ src/com/udayan/oca/Test.java
@@ -2,8 +2,15 @@ package com.udayan.oca;
 
 
 public class Test  {
+    public int x;
+
+    final static void met(){
+        System.out.println("ddddddddddddddd");
+    }
 
     public static void main(String ... a)  {
+
+        Test.met();
 
         String[] strs = new String[2];
         int idx=0;
