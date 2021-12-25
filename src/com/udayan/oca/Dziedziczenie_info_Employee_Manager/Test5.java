a86041dd64e977592df0c2ced4da3a5f9d461e44
Jacek <jaceksysiak@wp.pl>
2021-12-25 16:28:20 +0100

    25-12-2021 06:59

--- src/com/udayan/oca/Dziedziczenie_info_Employee_Manager/Test5.java
+++ src/com/udayan/oca/Dziedziczenie_info_Employee_Manager/Test5.java
@@ -7,15 +7,39 @@ class A {
 }
 
 class B extends A {
+    @Override
     public void test(){
         System.out.println("BBBBBBBBBBBBBBBBBB");
     }
 }
 
 class C extends A {
+    @Override
     public void test(){
         System.out.println("CCCCCCCCCCCCCCCCCCC");
     }
 }
 public class Test5 {
+
+    public static void main(String ... s) {
+        A b1 = new A();   // pełny A
+        b1.test(); // AAAAAA
+        A b2 = new C();   // niepełny C
+        b2.test(); // CCCCCC
+        A b3 = new B();   // niepełny B
+
+        boolean x = b2 instanceof  B;  //false
+
+        // b3 = (B)b2;  com.udayan.oca.Dziedziczenie_info_Employee_Manager.C cannot be cast to com.udayan.oca.Dziedziczenie_info_Employee_Manager.B
+
+     //   b2 = b1; // b2 - pełny A
+     //   b2.test();  //AAAAAAAAAAAAA
+
+        b1 = b2;
+        b1.test();  //CCCCCCCCCCC
+
+
+        System.out.println("ssssssssssssssssssssss");
+    }
+
 }
