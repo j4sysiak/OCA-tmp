5faa85ea2b59118bad21bb53ed92ce1c1ad7292c
Jacek <jaceksysiak@wp.pl>
2022-01-20 08:34:28 +0100

    20-01-2022 06:129

--- src/com/udayan/oca/Test16.java
+++ src/com/udayan/oca/Test16.java
@@ -2,28 +2,20 @@ package com.udayan.oca;
 
 
 public class Test16 {
+    public int price = 15;
+    private static boolean sold;
+    static final int CASH =100;
+
+
+    static {
+        sold = false;
+    }
+
     public static void main(String[] args) {
-        Order order = new Order(1);
-        System.out.println("I want to order a " + order.getMyOrder());
-    }
-}
-
-class Order {
-    public String getMyOrder() {
-        return myOrder;
-    }
-
-    private String myOrder;
-
-    Order(int x) {
-        switch (x) {
-            case 1:
-                this.myOrder = "steak";
-            case 2:
-                this.myOrder = "pizza";
-                break;
-            default:
-                this.myOrder = "salad";
+        if(CASH > price && !sold){
+            System.out.println("You have bought an apple");
+        } else {
+            System.out.println("All apples have been sold");
         }
     }
 }
