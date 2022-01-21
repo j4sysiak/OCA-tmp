18068b2bdf44177a5ab31fea85d55182577f5ee4
Jacek <jaceksysiak@wp.pl>
2022-01-21 06:41:34 +0100

    21-01-2022 06:11

---
+++ src/com/udayan/oca/A/B/C/D/AnimalX.java
@@ -0,0 +1,8 @@
+package com.udayan.oca.A.B.C.D;
+
+public class AnimalX {
+    public static String HEALTH;
+    static{
+        HEALTH="OK";
+    }
+}
--- src/com/udayan/oca/Test17.java
+++ src/com/udayan/oca/Test17.java
@@ -2,7 +2,13 @@ package com.udayan.oca;
 
 public class Test17 {
     public static void main(String[] args) {
-
-        System.out.println("ssssssssssssss");
+        int[] arr = null;// = new long[]{1,2,3,4,5};
+        long i = 5;
+        try {
+       //     arr = null;
+            System.out.println(arr[11111111]);
+        } catch (Exception e) {
+            System.out.println();
+        }
     }
 }
