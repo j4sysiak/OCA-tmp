7db30306d9c63e8f0d886d46f77dd85e39c87716
Jacek <jaceksysiak@wp.pl>
2022-01-22 17:55:19 +0100

    22-01-2022 05:44

--- src/com/udayan/oca/Test27.java
+++ src/com/udayan/oca/Test27.java
@@ -1,28 +1,20 @@
 package com.udayan.oca;
 
-public class Test27 {
+interface A {
+    int a = 1;
 
+    void run(int speed);
+}
+
+public class Test27 implements A {
+    int a = 5;
+
+    public void run(int a) {
+        System.out.println("Running with " + this.a + " mph");
+    }
 
     public static void main(String[] args) {
-
-     int a=1;
-    int b=a++;
-    int c=a++;
-
-    //a=3
-    //b=1
-    //c=2
-
-        try {
-            int i = 2;
-            int arr[][][][] = new int[i][i++][i++][i++];  //[2][2][3][4]
-            System.out.println("i = " + i);
-            System.out.print(arr.length);  // 2
-            System.out.print(arr[1].length);  //2
-            System.out.print(arr[1][1].length);  //3
-            System.out.print(arr[1][1][2].length); //4
-        } catch (IndexOutOfBoundsException e ){
-            System.out.println(e);
-        }
+        Test27 test = new Test27();
+        test.run(10);
     }
 }
