fac1952d48f6376ab077b16c9004be7cf335fd4b
Jacek <jaceksysiak@wp.pl>
2022-01-25 07:09:50 +0100

    25-01-2022 07:00

--- src/com/udayan/oca/Lambda/Test18.java
+++ src/com/udayan/oca/Lambda/Test18.java
@@ -1,20 +1,21 @@
 package com.udayan.oca.Lambda;
 
 interface Printer {
-    void print(int y);
+    void print(int abc);
 }
 
 public class Test18 {
     public static void main(String[] args) {
+
         Printer iprinter = new Printer() {
             @Override
-            public void print(int z) {
+            public void print(int a) {
                 System.out.println("Hello World");
             }
         };
 
         //1
-        metoda((int y) -> System.out.println("Hello World"));
+        metoda((int a) -> System.out.println("Hello World"));
 
         //2
         metoda(iprinter);
@@ -22,15 +23,15 @@ public class Test18 {
         //3
         metoda(new Printer() {
             @Override
-            public void print(int s) {
+            public void print(int a) {
                 System.out.println("Hello World");
             }
         });
         System.out.println("ssssssssssss");
     }
 
-    public static void metoda(Printer a) {
-        a.print(1);
+    public static void metoda(Printer ipr) {
+        ipr.print(1);
     }
 }
 
