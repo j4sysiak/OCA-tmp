02744fefda6955b4ed801071ecff7e1c94cd9322
Jacek <jaceksysiak@wp.pl>
2022-02-10 07:04:16 +0100

    10-02-2022 06:16

--- src/com/udayan/oca/Test41.java
+++ src/com/udayan/oca/Test41.java
@@ -1,4 +1,19 @@
 package com.udayan.oca;
 
-public class Test41 {
+interface Printer {
+    void met();
 }
+
+public class Test41 {
+    public static void main(String[] args) {
+        /*INSERT CODE*/
+
+        uuuu(() -> System.out.println("WWWWWWWWWWWWWWWWWWWWW"));
+
+    }
+
+    public static void uuuu(Printer a) {
+        a.met();
+    }
+}
+
