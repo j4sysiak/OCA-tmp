d39ae7a3ce5fc6f1d17974dcecc5e8277e42f40e
Jacek <jaceksysiak@wp.pl>
2022-02-12 05:32:36 +0100

    12-02-2022 06:04

--- src/com/udayan/oca/Test46.java
+++ src/com/udayan/oca/Test46.java
@@ -1,14 +1,12 @@
 package com.udayan.oca;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Test46 {
-    public static void main(String  args) {
-
-//        System.out.println(args);
-
-        customMethod();
-    }
-
-    private static void customMethod(String... strings) {
-        System.out.println("Object is: " + strings);
+    public static void main(String[] args) {
+        double a = 5;
+        boolean flag = a++ == 6 & --a == 5 || a++ ==5;
+        System.out.println(a+ " " + flag);
     }
 }
