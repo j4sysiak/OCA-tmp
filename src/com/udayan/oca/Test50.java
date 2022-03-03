076b4a11fa2d36298b9691fe8cd40cc4bb6d3395
Jacek <jaceksysiak@wp.pl>
2022-03-03 06:12:38 +0100

    03-03-2022 06:12

--- src/com/udayan/oca/Test50.java
+++ src/com/udayan/oca/Test50.java
@@ -1,28 +1,8 @@
 package com.udayan.oca;
 
-class Shape {
-    int x = 0;
-    int y = 5;
-}
-
-class Square extends Shape {
-    static int x = 20;
-           int y = 0;
-
-
+public class Test50 {
     public static void main(String[] args) {
-        Shape shape = new Square();
-
-        System.out.println(shape.x);
-        System.out.println(x);
-
         System.out.println("sssssssss");
 
-        Square square = (Square) shape;
     }
-
-}
-
-public class Test50 {
-
 }
