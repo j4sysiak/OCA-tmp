c6186c2b28a29a8d9cc9a6e7f0fc314e4337fc1d
Jacek <jaceksysiak@wp.pl>
2022-04-09 07:22:52 +0200

    09-04-2022 07:22

--- src/com/udayan/oca/Test55.java
+++ src/com/udayan/oca/Test55.java
@@ -2,14 +2,28 @@ package com.udayan.oca;
 
 public class Test55  {
     public static void main(String[] args) {
-        try {
-            System.out.println("12345".charAt(6));
-        } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Array");
-        } catch (IndexOutOfBoundsException e ){
-            System.out.println("Index");
-        } catch (StringIndexOutOfBoundsException e ){
-            System.out.println("String");
-        }
+
+        A[] listA = new A[10];
+        A a = new A();
+        A b = new B();
+        A c = new C();
+        ICustom i = new B();
+
+        listA[0] = a;
+        listA[1] = b;
+        listA[2] = c;
+
+        B[] listaB = new B[10];
+        listaB[0] = (B) i;
+
+        System.out.println("ssssssssssss");
+
+//        C c = new ICustom();
+//        ICustom d = a;
     }
 }
+
+interface ICustom {}
+class A {}
+class B extends A implements ICustom {}
+class C extends B{}
