f7b4db3c6566413f62c9fa612d6a62f1933fcd68
Jacek <jaceksysiak@wp.pl>
2022-04-10 07:04:19 +0200

    10-04-2022 07:04

--- src/com/udayan/oca/Test55.java
+++ src/com/udayan/oca/Test55.java
@@ -1,29 +1,9 @@
 package com.udayan.oca;
 
-public class Test55  {
-    public static void main(String[] args) {
-
-        A[] listA = new A[10];
-        A a = new A();
-        A b = new B();
-        A c = new C();
-        ICustom i = new B();
-
-        listA[0] = a;
-        listA[1] = b;
-        listA[2] = c;
-
-        B[] listaB = new B[10];
-        listaB[0] = (B) i;
-
-        System.out.println("ssssssssssss");
-
-//        C c = new ICustom();
-//        ICustom d = a;
-    }
+public abstract class Test55  {
+    final static private double secretNumber = 777.7_____0; //1
+    abstract void Integer(); //2
+    int b ; //3
+    Float[ ] obj = new Double[1]; //4
+    static   int a ; //5
 }
-
-interface ICustom {}
-class A {}
-class B extends A implements ICustom {}
-class C extends B{}
