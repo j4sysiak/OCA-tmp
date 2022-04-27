78e6b211d9a4cee323e8a129d4bb78a7cac82279
Jacek <jaceksysiak@wp.pl>
2022-04-27 07:06:25 +0200

    27-04-2022 07:06

--- src/com/udayan/oca/Test56.java
+++ src/com/udayan/oca/Test56.java
@@ -1,36 +1,21 @@
 package com.udayan.oca;
 
-
-import com.udayan.oca.modyfikatory_dostepu.otherPackage.A;
-
-import java.util.ArrayList;
-
 public class Test56 {
     public static void main(String[] args) {
-        Abstr aa = new Abstr() {};
-
-        ArrayList<A> list = new ArrayList<>();
-
-        list.add(new SubClass()); //1
-
-        list.add(new Class()); //2
 
 
-        list.add(new A() {}); //3
-
-        B a = new Class();
-     //   list.add(a);           //4
+        System.out.println("ssssssssssssss");
     }
 }
 
-abstract class Abstr {
+interface ICustom {
 }
 
-interface B {
+class A {}
+
+class B extends A implements ICustom {
 }
 
-class Class extends A implements B {
+class C extends B  {
 }
 
-class SubClass extends Class {
-}
